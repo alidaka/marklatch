9264a5ee69d46fe961ad0570615b4d299607a32e
alidaka <alidaka@gmail.com>
2017-04-22 17:10:52 -0400

    wip

--- app/dictionary.rb
+++ app/dictionary.rb
@@ -1,15 +1,30 @@
 class Dictionary
-  attr_reader :n
+  attr_reader :n, :chain
   def initialize(n=nil)
     @n = n.nil? ? 2 : n
-    @chain = {}
+    @chain = Hash.new {|hash, key| hash[key] = WordNode.new(key)}
   end
 
   def read(filename)
-
   end
 
   def add_text(text)
+    w_1 = w_2 = nil
+    text.split.each do |w|
+      if text == 'a b c' then require 'pry'; binding.pry end
+      case w
+      when w_1.nil?
+        chain[w]
+      when w_2.nil?
+        chain[w_1].children[w]
+      else
+        WordNode.new('$')
+      end.increment
+      if text == 'a b c' then require 'pry'; binding.pry end
+
+      w_2 = w_1
+      w_1 = w
+    end
   end
 
   def speak
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -32,9 +32,37 @@ describe Dictionary do
   end
 
   describe 'when parsing text' do
+    before do
+      @d = Dictionary.new(3)
+    end
+
     it 'handles a simple case' do
       @d.add_text('hello world')
       @d.speak.must_equal 'hello world'
+    end
+
+    it 'splits words' do
+      @d.add_text('a b c')
+
+      @d.chain.size.must_equal(1)
+      @d['a'].frequency.must_equal(1)
+      @d['a'].frequency(2).must_equal(4)
+      @d['a'].children.size.must_equal(3)
+      @d['a'].children.map{|w| w.word}.must_contain('b')
+      @d['a'].children.map{|w| w.word}.must_contain('c')
+      @d['a'].children.map{|w| w.word}.must_contain('.')
+    end
+
+    it 'splits words with punctuation' do
+      @d.add_text('a b. c')
+    end
+
+    it 'counts correctly' do
+      @d.add_text('a b. c b. a c')
+    end
+
+    it 'position matters' do
+      @d.add_text('a b. b')
     end
   end
 end
